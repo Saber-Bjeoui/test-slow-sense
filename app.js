0db5e2bc2920b8f972e3d2256c82262879865d77
Saber Bjeoui <bjeoui.saber@gmail.com>
2022-09-03 12:20:12 +0100

    set db connection in seperate file

--- app.js
+++ app.js
@@ -4,56 +4,7 @@ const port = process.env.PORT || 3000
 
 
 
-const Connection = require('tedious').Connection
-const Request = require('tedious').Request
 
-const config = {
-  server: 'slowsense.database.windows.net',
-  options: {
-    database: "ShapesTest"
-  },
-  authentication: {
-    type: 'default',
-    options: {
-      userName: 'SlowSenseTest', // update me
-      password: 'slow-test-55' // update me
-    }
-  }
-}
-
-const connection = new Connection(config)
-
-connection.on('connect', (err) => {
-    console.log('Connecting...')
-  if (err) {
-    console.log(err)
-  } else {
-    executeStatement()
-  }
-})
-
-function executeStatement () {
-    request = new Request("select * from Shapes", (err, rowCount) => {
-      if (err) {
-        console.log(err)
-      } else {
-        console.log(`${rowCount} rows`)
-      }
-      connection.close()
-    })
-  
-    request.on('row', (columns) => {
-      columns.forEach((column) => {
-        if (column.value === null) {
-          console.log('NULL')
-        } else {
-          console.log(column.value)
-        }
-      })
-    })
-  
-    connection.execSql(request)
-  }
 
 app.get('/shapes', (req, res) => {
     
--- db.js
+++ db.js
@@ -1,4 +1,27 @@
+const Connection = require('tedious').Connection
+const Request = require('tedious').Request
 
-
-
-exports = connection;
+const config = {
+    server: 'slowsense.database.windows.net',
+    options: {
+      database: "ShapesTest"
+    },
+    authentication: {
+      type: 'default',
+      options: {
+        userName: 'SlowSenseTest', // update me
+        password: 'slow-test-55' // update me
+      }
+    }
+  }
+  
+const connection = new Connection(config)
+  
+connection.on('connect', (err) => {
+    console.log('Connecting...')
+    if (err) {
+        console.log(err)
+    } 
+})
+  
+exports = connection
