d8099579d76cb96547be20fbf9fcf861d2915e8b
Saber Bjeoui <bjeoui.saber@gmail.com>
2022-09-03 17:16:50 +0100

    Feature: implement ShapeComponent

---
+++ web-app-front/src/app/shape-type.enum.ts
@@ -0,0 +1,5 @@
+export enum EShapeType {
+  CIRCLE = 1,
+  RECTANGLE,
+  TEXT
+}
--- web-app-front/src/app/shape/shape.component.ts
+++ web-app-front/src/app/shape/shape.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { EShapeType } from '../shape-type.enum';
 
 @Component({
   selector: 'app-shape',
   templateUrl: './shape.component.html',
   styleUrls: ['./shape.component.scss']
 })
-export class ShapeComponent implements OnInit {
+export class ShapeComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @Input() shapeType = EShapeType.TEXT
+  @Input() height =  0;
+  @Input() width = 0;
+  @Input() positionX = 0;
+  @Input() positionY = 0;
+  @Input() text = '';
 
-  ngOnInit(): void {
+  @ViewChild('shape') shape: ElementRef = new ElementRef('');
+
+  EShapeType = EShapeType
+
+  constructor(private elRef:ElementRef) { }
+
+  ngOnInit(): void { }
+
+  ngAfterViewInit() {
+
+    this.elRef.nativeElement.style.left = this.positionX + 'px';
+    this.elRef.nativeElement.style.top = this.positionY + 'px';
+
+    if(this.shapeType === EShapeType.CIRCLE) this.height = this.width
+
+    this.shape.nativeElement.style.width = this.width + 'px';
+    this.shape.nativeElement.style.height = this.height + 'px';
+
+    this.shape.nativeElement.textContent = this.text
   }
 
 }
