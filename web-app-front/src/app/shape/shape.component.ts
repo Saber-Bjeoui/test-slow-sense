17e7a4865154d09624e94cb5ef9a0aaddae25826
Saber Bjeoui <bjeoui.saber@gmail.com>
2022-09-03 18:59:56 +0100

    Fix: set basic styling and fix shapes ordering

--- web-app-front/src/app/shape/shape.component.ts
+++ web-app-front/src/app/shape/shape.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterContentChecked, AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { EShapeType } from '../shape-type.enum';
 
 @Component({
@@ -14,6 +14,7 @@ export class ShapeComponent implements OnInit, AfterViewInit {
   @Input() positionX = 0;
   @Input() positionY = 0;
   @Input() text = '';
+  @Input() order = 0
 
   @ViewChild('shape') shape: ElementRef = new ElementRef('');
 
@@ -21,19 +22,43 @@ export class ShapeComponent implements OnInit, AfterViewInit {
 
   constructor(private elRef:ElementRef) { }
 
+
   ngOnInit(): void { }
 
   ngAfterViewInit() {
 
+    //this.elRef.nativeElement.style.transform = `translateX(${this.positionX}px)`;
+
+    if(this.shapeType === EShapeType.TEXT) {
+      console.log(this.positionX, this.positionY)
+      console.log(this.elRef)
+      console.log({width: this.elRef.nativeElement.offsetWidth, height: this.elRef.nativeElement.offsetHeight})
+      this.positionX = this.positionX - (this.elRef.nativeElement.offsetWidth / 2)
+      this.positionY = this.positionY - (this.elRef.nativeElement.offsetHeight / 2)
+
+      console.log(this.positionX, this.positionY)
+     
+    }
+
     this.elRef.nativeElement.style.left = this.positionX + 'px';
     this.elRef.nativeElement.style.top = this.positionY + 'px';
 
+    this.elRef.nativeElement.style.zIndex = this.order;
+
     if(this.shapeType === EShapeType.CIRCLE) this.height = this.width
+
+   
 
     this.shape.nativeElement.style.width = this.width + 'px';
     this.shape.nativeElement.style.height = this.height + 'px';
 
+   
+
     this.shape.nativeElement.textContent = this.text
+  }
+
+  showPopup() {
+
   }
 
 }
